7f9055987867be1d4fa54336ef12983d171cc763
Naing Linn Phyo <nianglinnphyo24@gmail.com>
2023-07-11 11:35:43 +0630

    feat 🔥(app): fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
           <motion.div className='px-4 md:px-8 h-screen flex flex-col items-center md:pt-20  md:max-w-2xl lg:max-w-6xl mx-auto' initial={{ translateY: -100 }} animate={{ translateY: 0 }} exit={{ opacity: 0 }}>
                <div className=' md:flex h-full items-center md:text-start text-center flex-col-reverse flex md:flex-row max-w-4xl mx-auto'>
                     <div className='md:mt-0 mt-2 w-full overflow-hidden'>
-                         <motion.div animate={{ y: 0 }} initial={{ y: '100%' }} transition={{ delay: 0.5, duration: 0.5 }}>
+                         <motion.div className='mt-2 md:mt-0' animate={{ y: 0 }} initial={{ y: '100%' }} transition={{ delay: 0.5, duration: 0.5 }}>
                               <h1 className='md:text-sm text-xl font-medium tracking-tight text-start '  >
                                    <pre className='md:text-[0.78rem] md:leading-4 text-[0.62rem] leading-3'>
                                         <code className='text-start'>
@@ -45,7 +45,7 @@ function Hero() {
                          <div className='w-12 h-12 top-28 right-2 absolute text-xl -rotate-12'>
                               <Image src={'/code.svg'} alt={''} width={100} height={100} className='w-full h-full' />
                          </div>
-                         <div className='md:w-full md:h-full w-[100%] h-[100%]'>
+                         <div className='md:w-full md:h-full w-[95%] h-[95%]'>
                               <Image src='/img/LastMyPic.png' width={500} height={500} alt='' className='w-full h-full' />
                          </div>
                          <div className='w-60 h-8 md:w-64 border border-[#acb2bf] flex items-center'>
