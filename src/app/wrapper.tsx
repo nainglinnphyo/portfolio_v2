f4c9f6412085038e39c7d2fd8cd199032a71f775
Naing Linn Phyo <nianglinnphyo24@gmail.com>
2023-07-11 21:09:48 +0630

    feat 🔥(app): mosuse update

--- src/app/wrapper.tsx
+++ src/app/wrapper.tsx
@@ -6,31 +6,46 @@ import React, { useEffect, useState } from 'react'
 
 function Wrapper({ children }: { children: React.ReactNode }) {
 
-     const cursorX = useMotionValue(-100);
-     const cursorY = useMotionValue(-100);
-     const springConfig = { damping: 30, stiffness: 400 };
+     const backWardX = useMotionValue(-100);
+     const backWardY = useMotionValue(-100);
+     const backWardSpringConfig = { damping: 30, stiffness: 180 };
+     const backWardXSpring = useSpring(backWardX, backWardSpringConfig);
+     const backWardYSpring = useSpring(backWardY, backWardSpringConfig);
+
+     const cursorX = useMotionValue(0);
+     const cursorY = useMotionValue(0);
+     const springConfig = { damping: 30, stiffness: 500 };
      const cursorXSpring = useSpring(cursorX, springConfig);
      const cursorYSpring = useSpring(cursorY, springConfig);
 
      useEffect(() => {
           const moveCursor = (e: any) => {
-               cursorX.set(e.clientX - 16)
-               cursorY.set(e.clientY - 16)
+               cursorX.set(e.clientX - 0)
+               cursorY.set(e.clientY - 0)
+          };
+          const backWardMoveCursor = (e: any) => {
+               backWardX.set(e.clientX - 12)
+               backWardY.set(e.clientY - 12)
           };
           window.addEventListener('mousemove', moveCursor)
-          window.addEventListener('mouseenter', moveCursor)
+          window.addEventListener('mousemove', backWardMoveCursor)
+          // window.addEventListener('mouseenter', moveCursor)
           return () => {
+               window.removeEventListener('mousemove', backWardMoveCursor)
                window.removeEventListener('mousemove', moveCursor)
           }
      }, [])
      return (
           <div>
-               <motion.div className={`pointer-events-none w-8 h-8 rounded-full border border-[#abb1be] items-center  justify-center fixed z-[9999] hidden md:flex`} style={{
+               <motion.div className='w-2 h-2 bg-[#abb1be] rounded-full absolute z-[9999]' style={{
                     translateX: cursorXSpring,
                     translateY: cursorYSpring,
                }}>
-                    <motion.div className='w-1 h-1 bg-[#abb1be] rounded-full'>
-                    </motion.div>
+               </motion.div>
+               <motion.div className={`pointer-events-none w-8 h-8 rounded-full border border-[#abb1be] z-[9999] absolute`} style={{
+                    translateX: backWardXSpring,
+                    translateY: backWardYSpring,
+               }}>
                </motion.div>
                <NavBar />
                {children}
