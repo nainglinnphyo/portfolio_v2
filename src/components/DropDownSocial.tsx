7313769b8a4662fa94707960a22b3e2ee83dddb1
Naing Linn Phyo <nianglinnphyo24@gmail.com>
2023-07-11 12:57:25 +0630

    feat 🔥(app): fixed

--- src/components/DropDownSocial.tsx
+++ src/components/DropDownSocial.tsx
@@ -5,22 +5,22 @@ import React from 'react'
 function DropDownSocial() {
      return (
           <div className='left-5 top-0 md:block hidden fixed z-50 '>
-               <div className='h-48 w-[0.08rem] bg-[#acb2bf]'>
+               <div className='h-40 w-[0.125rem] bg-[#acb2bf]'>
                </div>
-               <div className='absolute -left-4 mt-3 flex flex-col space-y-2'>
+               <div className='absolute -left-3 mt-2 flex flex-col space-y-2'>
                     <Link href='https://github.com/nainglinnphyo' target='_blank'>
                          <Icon icon='mdi:github' color='#acb2bf'
-                              className={`h-8 w-8`}
+                              className={`h-6 w-6`}
                          />
                     </Link>
                     <Link href='https://www.linkedin.com/in/naing-linn-phyo-768852175/' target='_blank'>
                          <Icon icon='entypo-social:linkedin-with-circle' color='#acb2bf'
-                              className={`h-8 w-8`}
+                              className={`h-6 w-6`}
                          />
                     </Link>
                     <Link href='https://telegram.me/defined_me' target='_blank'>
                          <Icon icon='ic:baseline-telegram' color='#acb2bf'
-                              className={`h-8 w-8`}
+                              className={`h-6 w-6`}
                          />
                     </Link>
                </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
           <motion.div className='px-4 md:px-8 h-screen flex flex-col items-center md:pt-20  md:max-w-2xl lg:max-w-6xl mx-auto' initial={{ translateY: -100 }} animate={{ translateY: 0 }} exit={{ opacity: 0 }}>
                <div className=' md:flex h-full items-center md:text-start text-center flex-col-reverse flex md:flex-row max-w-4xl mx-auto'>
                     <div className='md:mt-0 mt-2 w-full overflow-hidden'>
-                         <motion.div className='mt-2 md:mt-0' animate={{ y: 0 }} initial={{ y: '100%' }} transition={{ delay: 0.5, duration: 0.5 }}>
+                         <div className='mt-2 md:mt-0'>
                               <h1 className='md:text-sm text-xl font-medium tracking-tight text-start '  >
                                    <pre className='md:text-[0.78rem] md:leading-4 text-[0.62rem] leading-3'>
                                         <code className='text-start'>
@@ -26,9 +26,9 @@ function Hero() {
                                    <TypeAnimation
                                         className='md:text-xl text-base'
                                         sequence={[
-                                             'I build & develop web app 🌐',
+                                             'I build & develop web app!',
                                              2000,
-                                             'I build & develop cross platform 📱',
+                                             'I build & develop cross platform!',
                                              2000,
                                         ]}
                                         style={{ color: '#21c55d' }}
@@ -39,7 +39,7 @@ function Hero() {
                               <h1 className='md:text-sm text-xs mt-4 text-[#9398a4] text-start'>
                                    I'm a skilled fullstack developer proficient <br /> in JavaScript, TypeScript, PHP, and Golang.
                               </h1>
-                         </motion.div>
+                         </div>
                     </div>
                     <div className='flex-col flex items-center md:w-1/3 relative '>
                          <div className='w-12 h-12 top-28 right-2 absolute text-xl -rotate-12'>
