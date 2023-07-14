05dbfa9266c338f95493edd1c9c9bc9d436f63d8
Naing Linn Phyo <nianglinnphyo24@gmail.com>
2023-07-14 13:45:49 +0630

    feat 🔥(app): fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 
 function Hero() {
      return (
-          <motion.div className="px-4 md:px-8 h-full md:mb-48 mb-40 flex flex-col items-center md:pt-20  md:max-w-2xl lg:max-w-6xl mx-auto bg-[url('/img/bg.svg')]" initial={{ translateY: -100 }} animate={{ translateY: 0 }} exit={{ opacity: 0 }}>
+          <motion.div className="px-4 md:px-8 min-h-[700px] h-screen flex flex-col items-center md:pt-20  md:max-w-2xl lg:max-w-6xl mx-auto bg-[url('/img/bg.svg')]" initial={{ translateY: -100 }} animate={{ translateY: 0 }} exit={{ opacity: 0 }}>
                <div className=' md:flex items-center md:text-start text-center flex-col-reverse flex md:flex-row max-w-4xl mx-auto'>
                     <div className='md:mt-0 mt-2 w-full overflow-hidden'>
                          <div className='mt-2 md:mt-0'>
@@ -56,7 +56,7 @@ function Hero() {
                          </div>
                     </div>
                </div>
-               <div className='md:mt-32 mt-4 relative max-w-xl mx-auto'>
+               <div className='md:mt-32 mt-4 relative'>
                     <Image src='/img/QuotOne.svg' width={15} height={15} alt={''} className='absolute -top-2 z-10 bg-[#282C34] left-2' />
                     <Image src='/img/QuotOne.svg' width={15} height={15} alt={''} className='absolute right-8 -bottom-1 z-10 bg-[#282C34]' />
                     <div className='h-12 text-[0.7rem] md:text-lg border border-[#acb2bf] md:max-w-[600px] mx-auto'>
@@ -66,14 +66,15 @@ function Hero() {
                     <span className='h-12 text-[0.65rem] md:text-sm border border-[#acb2bf] border-t-0 right-0 md:max-w-[200px] mx-auto absolute w-40 md:w-48' >
                          <h1 className='py-4 md:py-3 px-2'>What Can i help you❓</h1>
                     </span>
-               </div>
-               <motion.div className='w-5 h-9 rounded-3xl border border-[#acb2bf] absolute bottom-6 flex items-center justify-center' initial={{ opacity: 0, }} animate={{ opacity: 1, }} exit={{ opacity: 0 }} >
-                    <motion.div className='w-[0.4rem] h-[0.4rem] rounded-full bg-[#acb2bf] mb-5'
-                         animate={{ y: [0, 20, 0] }}
-                         transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
-                    >
+                    <motion.div className='left-1/2 w-5 h-9 rounded-3xl -bottom-32 border border-[#acb2bf] absolute flex items-center justify-center' initial={{ opacity: 0, }} animate={{ opacity: 1, }} exit={{ opacity: 0 }} >
+                         <motion.div className='w-[0.4rem] h-[0.4rem] rounded-full bg-[#acb2bf] mb-5'
+                              animate={{ y: [0, 20, 0] }}
+                              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
+                         >
+                         </motion.div>
                     </motion.div>
-               </motion.div>
+               </div>
+
           </motion.div >
      )
 }
